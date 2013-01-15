f6fe32e6aff35decd37745e79aac3dfb39d2288d
Pavel Shut <sp.shut@gmail.com>
2013-01-15 12:12:22 +0300

    add basic files for page switching

---
+++ js/pages.js
@@ -0,0 +1,64 @@
+$(function(){
+	XF.start({
+
+		settings: {
+			applicationVersion: '1.1'+ Math.random(),
+			noCache: true,
+			componentUrlPrefix: 'js/components/',
+			templateUrlPrefix: 'tmpl/',
+			dataUrlPrefix: 'mocks/'
+
+		},
+
+		device: {
+			types : [{
+					name : 'tablet',
+					range : {
+						max : null,
+						min : 569
+					},
+					templatePath : 'tablet/',
+					fallBackTo : 'default'
+				}, {
+					name : 'mobile',
+					range : {
+						max : 568,
+						min : null
+					},
+					templatePath : 'mobile/',
+					fallBackTo : 'default'
+			}]
+		},
+
+		router: {
+			routes: {
+				'': 			'home',
+				'home2': 			'home2',
+				'home3': 			'home3',
+				'/*action/':	'action'
+			},
+
+			handlers: {
+				home: function () {
+					$('.xf-header > .xf-button-header-left').hide();
+					XF.Controller.bind('app-header-component:constructed', function () {
+						$('.xf-header > .xf-button-header-left').hide();
+					});
+
+					if (XF.Device.type.name !== 'mobile') XF.Controller.trigger('menu:go', {hash: 'about'});
+
+				},
+
+				action : function(hash) {
+					if (hash == 'about' && XF.Device.type.name !== 'mobile') $('.xf-header > .xf-button-header-left').hide();
+					else $('.xf-header > .xf-button-header-left').show();
+
+
+					XF.Controller.trigger('menu:go', {hash: hash});
+				}
+			}
+		}
+
+	});
+
+});
