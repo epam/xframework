759cefdd8242286bff85109e7607b0621a8cdef5
Mikhail Larchanka <mikhail.larchanka@gmail.com>
2013-08-23 14:41:11 +0300

    CSS minifying added

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,6 +1,6 @@
-module.exports = (function(grunt) {
+module.exports = (function (grunt) {
 
-    grunt.registerTask('build', "X-Framework build", function() {
+    grunt.registerTask('build', "X-Framework build", function () {
         console.log('Adding core elements');
         var jsSources = [
             'xf/src/xf.core.js',
@@ -57,22 +57,25 @@ module.exports = (function(grunt) {
                 }
             },
             less: {
-              development: {
-                options: {
-                  paths: ["styles"]
+                development: {
+                    options: {
+                        paths: ["styles"]
+                    },
+                    files: {
+                        "styles/xf.css": "styles/xf.less"
+                    }
                 },
-                files: {
-                  "styles/xf.css": "styles/xf.less"
+                production: {
+                    options: {
+                        paths: ["styles"],
+                        compress: true,
+                        yuicompress: true,
+                        report: 'min'
+                    },
+                    files: {
+                        "styles/xf.min.css": "styles/xf.less"
+                    }
                 }
-              },
-              production: {
-                options: {
-                  paths: ["styles"]
-                },
-                files: {
-                  "styles/xf.css": "styles/xf.less"
-                }
-              }
             }
         });
         grunt.loadNpmTasks('grunt-contrib-uglify');
