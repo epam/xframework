b75649a14934170554c81911d91c4c9972ec7d78
Mikhail Larchanka <horrorgr@gmail.com>
2013-12-11 17:41:43 +0300

    Gruntfile changed

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,132 +1,13 @@
-module.exports = function (grunt) {
-    
-    grunt.registerTask('build', "X-Framework build", function () {
-        console.log('Adding core elements');
-        var jsSources = [
-        'xf/src/xf.jquery.hooks.js',
-        'xf/src/xf.core.js',
-        'xf/src/xf.app.js',
-        'xf/src/xf.touch.js',
-        'xf/src/xf.router.js',
-        'xf/src/xf.utils.js',
-        'xf/src/xf.pages.js',
-        'xf/src/xf.ui.js',
-        'xf/src/xf.settings.js',
-        'xf/src/xf.storage.js',
-        'xf/src/xf.device.js',
-        'xf/src/xf.collection.js',
-        'xf/src/xf.model.js',
-        'xf/src/xf.view.js',
-        'xf/src/xf.component.js'
-        ];
 
-        // Run through files and detect icons to use
-        //var lessSources = [];
+module.exports = function(grunt) {
 
-        // License text
-
-        var license = '';//fs.readFileSync('./LICENSE.txt');
-
-
-        // TODO modules to add
-
-        if (arguments.length === 0) {
-            console.log('Adding all ui components');
-            jsSources.push('xf/ui/*.js');
-        } else {
-
-            for (var i in arguments) {
-                console.log('Adding ui for "' + arguments[i] + '"');
-                jsSources.push('xf/ui/xf.ui.' + arguments[i] + '.js');
-            }
-        }
-
-        grunt.initConfig({
-            pkg: grunt.file.readJSON('package.json'),
-            build: {
-                all: {
-                    dest: "js/xf.js",
-                    minimum: [
-                        "core"
-                    ]
-                }
-            },
-        
-            uglify: {
-                dist: {
-                    files: {
-                        'js/xf.min.js': ['<%= build.all.dest %>']
-                    }
-                }
-            },
-            less: {
-                development: {
-                    options: {
-                        paths: ["styles"]
-                    },
-                    files: {
-                        "styles/xf.css": "styles/xf.less"
-                    }
-                }
-            },
-            recess: {
-                pretify: {
-                    options: {
-                        compile: true,
-                    },
-                    files: {
-                        "styles/xf.css" : ["styles/xf.css"]
-                    }
-                },
-                minify: {
-                    options: {
-                        compile: true,
-                        compress: true
-                    },
-                    files: {
-                        "styles/xf.min.css" : ["styles/xf.css"]
-                    }
-                }
-            }
-        });
-        grunt.loadNpmTasks('grunt-contrib-uglify');
-        grunt.loadNpmTasks('grunt-contrib-concat');
-        grunt.loadNpmTasks('grunt-contrib-less');
-        grunt.loadNpmTasks('grunt-recess');
-        
-
-
-        grunt.loadNpmTasks('grunt-bower-task');
-
-
-        // Load grunt tasks from NPM packages
-        require( "load-grunt-tasks" )( grunt );
-
-        // Integrate jQuery specific tasks
-        grunt.loadTasks( "build/tasks" );
-        
-        grunt.task.run(['build', 'uglify', 'less', 'recess']);
-            
-    });
-
-    grunt.registerTask('test', "X-Framework qunit test", function () {
+    grunt.registerTask('test', "X-Framework qunit test", function() {
         grunt.initConfig({
             jshint: {
                 options: {
                     jshintrc: '.jshintrc'
                 },
-                files: [
-                'Gruntfile.js',
-                'package.json',
-                'xf/src/*.js',
-                'xf/ui/*.js',
-                'js/xf.js',
-                'test/components/test.js',
-                'test/src/*.js',
-                'test/ui/*.js',
-                'test/lib/run-qunit.js',
-                'test/*.js'
-                ]
+                files: ['Gruntfile.js', 'package.json', 'xf/src/*.js', 'xf/ui/*.js', 'js/xf.js', 'test/components/test.js', 'test/src/*.js', 'test/ui/*.js', 'test/lib/run-qunit.js', 'test/*.js']
             },
             recess: {
                 options: {
@@ -135,9 +16,7 @@ module.exports = function (grunt) {
                     noOverqualifying: false,
                     zeroUnits: false
                 },
-                files:[
-                'styles/*.css'
-                ]
+                files: ['styles/*.css']
             },
             qunit: {
                 files: ['test/index.html']
@@ -149,10 +28,72 @@ module.exports = function (grunt) {
         grunt.task.run(['jshint', 'qunit', 'recess']);
     });
 
+    grunt.loadNpmTasks('grunt-bower-task');
+
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        build: {
+            all: {
+                dest: "js/xf.js",
+                minimum: ["core"]
+            },
+        },
+        bower: {
+            install: {
+                options: {
+                    targetDir: "bower_modules",
+                    cleanup: true
+                }
+            }
+        },
+        uglify: {
+            dist: {
+                files: {
+                    'js/xf.min.js': ['<%= build.all.dest %>']
+                }
+            }
+        },
+        less: {
+            development: {
+                options: {
+                    paths: ["styles"]
+                },
+                files: {
+                    "styles/xf.css": "styles/xf.less"
+                }
+            }
+        },
+        recess: {
+            pretify: {
+                options: {
+                    compile: true,
+                },
+                files: {
+                    "styles/xf.css": ["styles/xf.css"]
+                }
+            },
+            minify: {
+                options: {
+                    compile: true,
+                    compress: true
+                },
+                files: {
+                    "styles/xf.min.css": ["styles/xf.css"]
+                }
+            }
+        }
+    });
+
+    // Load grunt tasks from NPM packages
+    require("load-grunt-tasks")(grunt);
+
+    // Integrate jQuery specific tasks
+    grunt.loadTasks("build/tasks");
+
 
     grunt.registerTask('install', ['bower']);
 
 
-    grunt.registerTask('default', ['build']);
+    grunt.registerTask('default', ['build', 'uglify', 'less', 'recess']);
 
-};
+}; 
