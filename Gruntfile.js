f989f6d7d5206a29966e76dd3b9357a16886af9a
Mikhail Larchanka <horrorgr@gmail.com>
2013-12-13 10:23:00 +0300

    Additional grunt task added [Gruntfile.js] Tak for application building added [package.json] proccesshtml dependency added

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,23 +1,23 @@
 
 module.exports = function(grunt) {
 
-    grunt.registerTask('test', "X-Framework qunit test", function() {
+    grunt.registerTask('test', "XFramework qunit test", function() {
         grunt.initConfig({
             jshint: {
                 options: {
                     jshintrc: '.jshintrc'
                 },
                 files: [
-                    'Gruntfile.js', 
-                    'package.json', 
-                    'xf/src/*.js', 
-                    'xf/ui/*.js', 
-                    'js/xf.js', 
-                    'test/components/test.js', 
-                    'test/src/*.js', 
-                    'test/ui/*.js', 
-                    'test/lib/run-qunit.js', 
-                    'test/*.js'
+                'Gruntfile.js', 
+                'package.json', 
+                'xf/src/*.js', 
+                'xf/ui/*.js', 
+                'js/xf.js', 
+                'test/components/test.js', 
+                'test/src/*.js', 
+                'test/ui/*.js', 
+                'test/lib/run-qunit.js', 
+                'test/*.js'
                 ]
             },
             recess: {
@@ -37,6 +37,58 @@ module.exports = function(grunt) {
         grunt.loadNpmTasks('grunt-contrib-jshint');
         grunt.loadNpmTasks('grunt-recess');
         grunt.task.run(['jshint', 'qunit', 'recess']);
+    });
+    
+    grunt.registerTask('appbuild', "XFramework: application build", function() {
+        grunt.initConfig({
+            uglify: {
+                dist: {
+                    files: {
+                        'prod/js/app.min.js': ['js/app.js', 'js/components/*.js']
+                    }
+                },
+                lib: {
+                    files: {
+                        'prod/js/lib.min.js': [
+                        'js/lib/jquery/jquery.js', 
+                        'js/lib/underscore/underscore.js',
+                        'js/lib/backbone/backbone.js',
+                        'js/lib/requirejs/requirejs.js'
+                        ]
+                    }
+                }
+            },
+            recess: {
+                pretify: {
+                    options: {
+                        compile: true,
+                    },
+                    files: {
+                        "styles/app.css": ["styles/app.css"]
+                    }
+                },
+                minify: {
+                    options: {
+                        compile: true,
+                        compress: true
+                    },
+                    files: {
+                        "prod/styles/app.min.css": ["styles/xf.min.css", "styles/app.css"]
+                    }
+                }
+            },
+            processhtml: {
+                dist: {
+                    files: {
+                        'prod/index.html': ['index.html']
+                    }
+                }
+            }
+        });
+        grunt.loadNpmTasks('grunt-contrib-uglify');
+        grunt.loadNpmTasks('grunt-recess');
+        grunt.loadNpmTasks('grunt-processhtml');
+        grunt.task.run(['uglify', 'recess', "processhtml"]);
     });
 
     grunt.loadNpmTasks('grunt-bower-task');
@@ -101,9 +153,7 @@ module.exports = function(grunt) {
     // Integrate jQuery specific tasks
     grunt.loadTasks("build/tasks");
 
-
     grunt.registerTask('install', ['bower']);
-
 
     grunt.registerTask('default', ['build', 'uglify', 'less', 'recess']);
 
