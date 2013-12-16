605dff64ab487ca2506f7bab95d5383739b45609
Jauhen <jauhen@gmail.com>
2013-12-16 01:30:47 -0800

    Move XF.start from xf.core.js to app/start.js, add test for new module and make related changes.

---
+++ test/main.js
@@ -0,0 +1,37 @@
+require.config({
+    urlArgs: 'now=' + Date.now(),
+    paths: {
+        jquery: './../bower_modules/jquery/jquery',
+        underscore: './../bower_modules/underscore/underscore',
+        backbone: './../bower_modules/backbone/backbone',
+        sinon: './../bower_modules/sinonjs/sinon'
+    },
+    shim: {
+        'jquery': {
+            exports: 'jQuery'
+        },
+        'backbone': {
+            //These script dependencies should be loaded before loading
+            //backbone.js
+            deps: ['underscore', 'jquery'],
+            //Once loaded, use the global 'BB' as the
+            //module value.
+            exports: 'Backbone'
+        },
+        'underscore': {
+            exports: '_'
+        },
+        'sinon': {
+            exports: 'sinon'
+        }
+    }
+});
+
+requirejs([
+    'src/app/start'
+], function(/* remember: the test modules don't export anything */) {
+
+    // All the test files have been loaded, and all the tests have been
+    // defined--we're ready to start testing!
+    QUnit.start();
+});
