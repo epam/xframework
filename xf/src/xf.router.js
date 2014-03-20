57a1f2ecd25fdee14e33824aa2047e22c4eac677
larchanka@me.com <horrorgr@gmail.com>
2014-03-20 18:23:04 +0300

    ui:enchance fix for body element

--- xf/src/xf.router.js
+++ xf/src/xf.router.js
@@ -23,17 +23,19 @@ define([
      */
     XF.Router = BB.Router;
 
-    _.extend(XF.Router.prototype, /** @lends XF.Router.prototype */{
+    _.extend(XF.Router.prototype, /** @lends XF.Router.prototype */ {
 
 
         /**
          Initiates Rounting & history listening
          @private
          */
-        start : function(options) {
+        start: function(options) {
             this.bindAnyRoute();
             XF.history.start(options);
-            XF.trigger('ui:enhance', Dom.root);
+
+            // TODO pass Dom.root element instead of $(body)
+            XF.trigger('ui:enhance', $('body'));
         },
 
 
@@ -41,8 +43,8 @@ define([
          Binds a callback to any route
          @param {Function} callback A function to be called when any route is visited
          */
-        bindAnyRoute : function() {
-            this.on('route', function (e) {
+        bindAnyRoute: function() {
+            this.on('route', function(e) {
                 console.log('XF.router :: route: ', this.getPageNameFromFragment(XF.history.fragment));
                 if (XF.pages) {
                     XF.pages.show(this.getPageNameFromFragment(XF.history.fragment));
@@ -55,8 +57,8 @@ define([
          @param String fragment
          @return String
          */
-        getPageNameFromFragment : function(fragment) {
-            var parts = fragment.replace(/^\/+/,'').replace(/\/+$/,'').split('/');
+        getPageNameFromFragment: function(fragment) {
+            var parts = fragment.replace(/^\/+/, '').replace(/\/+$/, '').split('/');
             return parts[0];
         }
     });
